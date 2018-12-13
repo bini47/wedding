471afda44063b40cdcedccdc94f8356f1626f523
voodoo <nohashanon@gmail.com>
2018-12-13 11:10:17 -0800

    vendor moblile view optimized

--- public/js/vendor_owl.js
+++ public/js/vendor_owl.js
@@ -1,25 +1,15 @@
-
 $(document).ready(function() {
- 
-    
-    $("#news-slider5").owlCarousel({
-        items:3,
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[1000,3],
-        itemsMobile:[650,3],
-        pagination:true,
-        navigationText:true,
-        autoPlay:false,
-        mergeFit:false,
-        margin:0,
-        dotsEach:true,
-        slideBy:1,
-        
-        
-    });
-    
- 
+  $("#news-slider5").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [1000, 3],
+    itemsMobile: [650, 2],
+    pagination: true,
+    navigationText: true,
+    autoPlay: false,
+    mergeFit: false,
+    margin: 0,
+    dotsEach: true,
+    slideBy: 1
+  });
 });
-
-
-
